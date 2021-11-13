07971adbf864ddf007abebc97dd3ee21b8413cde
Kurt Neufeld <kneufeld@burgundywall.com>
2021-11-12 20:38:38 -0700

    adding a BackKeyMixin as example code

--- streamdeckui/mixins.py
+++ streamdeckui/mixins.py
@@ -1,9 +1,20 @@
 import logging
 logger = logging.getLogger(__name__)
 
+# these are mostly just example code and could easily be
+# in your custom Key classes
 
 class QuitKeyMixin:
 
     async def cb_key_up(self, *args, **kw):
         logger.info("you pushed the exit key")
         self.deck._quit_future.set_result(None)
+
+
+class BackKeyMixin:
+
+    async def cb_key_up(self, *args, **kw):
+        from .key import Key
+
+        self.state = Key.UP
+        self.deck.prev_page()
