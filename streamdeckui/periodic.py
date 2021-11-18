8a06c58bd6a3d18a2458be3231c6dd10cf971ab7
Kurt Neufeld <kneufeld@burgundywall.com>
2021-11-18 14:12:53 -0700

    adding Alternating class to extend Periodic

--- streamdeckui/periodic.py
+++ streamdeckui/periodic.py
@@ -4,7 +4,10 @@ import asyncio
 import inspect
 from contextlib import suppress
 from functools import partial
+import itertools
 
+import logging
+logger = logging.getLogger(__name__)
 
 def is_async(func):
     # python 3.8 changed how an async function is passed through a functools.partial
@@ -26,12 +29,16 @@ class Periodic:
         self._task      = None
 
     def start(self):
+        # logger.debug(f"{self.__class__.__name__}.start()")
+
         if not self.is_started:
             self.is_started = True
             # task to call func periodically
             self._task = self.loop.create_task(self._run())
 
     async def stop(self):
+        # logger.debug(f"{self.__class__.__name__}.stop()")
+
         if self.is_started:
             self.is_started = False
             # stop task and await it stopped:
@@ -45,8 +52,23 @@ class Periodic:
 
         while True:
             await asyncio.sleep(self.time)
+            # logger.debug(f"{self.__class__.__name__} tick {self.time}")
 
             if _is_async:
                 await func(*args, **kwargs)
             else:
                 func(*args, **kwargs)
+
+
+class Alternating(Periodic):
+    def __init__(self, loop, time, funcs):
+        """
+        funcs: a list of functions that are continously cycled through
+        """
+        super().__init__(loop, time, self.cycle)
+        self._next = itertools.cycle(funcs)
+
+    async def cycle(self):
+        cb = next(self._next)
+        # logger.debug(f"{self.__class__.__name__}.cycle() {cb}")
+        await cb()
